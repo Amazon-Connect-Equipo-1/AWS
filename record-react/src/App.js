060fe9321e8af66e2b6c37f49bf120788012549b
Jacqueline Zavala <a01750480@itesm.mx>
2022-05-17 15:13:06 -0500

    prueba S3

--- record-react/src/App.js
+++ record-react/src/App.js
@@ -1,4 +1,5 @@
 import { useReactMediaRecorder } from "react-media-recorder";
+import {S3} from "aws-sdk";
  
 const App = () => {
   const {
@@ -7,12 +8,50 @@ const App = () => {
     stopRecording,
     mediaBlobUrl,
   } = useReactMediaRecorder({ screen: true });
+
+  const processVideo = async (mediaBlobUrl) => {
+    stopRecording();
+
+    // Fetch del URL y conversion a blob
+    const mediaBlob = await fetch(mediaBlobUrl)
+      .then(response => response.blob());
+  
+    // File
+    let file = new File([mediaBlob], 'filename', { type: 'video/webm',    lastModified: Date.now() })
+    
+    await uploadFilesToS3('webm','videos', file,'myfile')
+    
+    async function uploadFilesToS3(extension,path,file,fileName) {
+      return new Promise(async (resolve, reject) => {
+        const bucket = new S3(
+          {
+            accessKeyId: "",
+            secretAccessKey: "",
+            region: ""
+          }
+        );
+        const params = {
+          Bucket: "screen-raw-recordings",
+          Key: path+ "/" + fileName+ extension,
+          Body: file
+        };
+        bucket.upload(params,async(err,data)=>{
+               if(data){
+                    console.log("Video uploaded")
+                 }
+                 if(err){
+                    console.log("Video uploaded failed")
+                 }
+           })
+      })
+    }
+  }
  
   return (
     <div>
       <p>{status}</p>
       <button onClick={startRecording}>Start Recording</button>
-      <button onClick={stopRecording}>Stop Recording</button>
+      <button onClick={processVideo}>Stop Recording</button>
       <video src={mediaBlobUrl} controls autoPlay loop />
     </div>
   );
