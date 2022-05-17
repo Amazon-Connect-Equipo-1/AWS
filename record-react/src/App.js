6db6bf2466fcee4b674687c1b7f0eb57b10e25c0
Bluehiperspace8 <58601367+Bluehiperspace8@users.noreply.github.com>
2022-05-17 14:04:12 -0500

    Segundo intento de folder

---
+++ record-react/src/App.js
@@ -0,0 +1,21 @@
+import { useReactMediaRecorder } from "react-media-recorder";
+ 
+const App = () => {
+  const {
+    status,
+    startRecording,
+    stopRecording,
+    mediaBlobUrl,
+  } = useReactMediaRecorder({ screen: true });
+ 
+  return (
+    <div>
+      <p>{status}</p>
+      <button onClick={startRecording}>Start Recording</button>
+      <button onClick={stopRecording}>Stop Recording</button>
+      <video src={mediaBlobUrl} controls autoPlay loop />
+    </div>
+  );
+};
+
+export default App;
