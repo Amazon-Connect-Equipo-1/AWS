a867fd042d25f62b8f50567e14a231d6bd93f42d
Erick Bustos <A01378966@itesm.mx>
2022-05-21 16:15:04 -0500

    Merge Audio and Video

--- Video Recording and Processing/JoinVideoAndAudio.py
+++ Video Recording and Processing/JoinVideoAndAudio.py
@@ -1,11 +1,20 @@
+'''
+    Code to join audio and video
+    JoinVideoAndAudio.py
+
+    Author:
+    - Erick Bustos.
+
+    Creation date: 16/05/2022
+    Last modification date: 16/05/2022
+'''
+
 # Import everything needed to edit video clips
 from moviepy.editor import *
 
 
 # loading video dsa gfg intro video
 clip = VideoFileClip("trial_video.MOV")
-
-
 
 # loading audio file
 audioclip = AudioFileClip("Nueva grabación 9.m4a").subclip(0, 30)
