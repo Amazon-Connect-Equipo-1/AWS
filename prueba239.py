1d3984d13f1ca8cc61826fbc2a29f0886c897a4e
Erick Bustos <A01378966@itesm.mx>
2022-05-22 10:49:18 -0500

    Clean files

--- prueba239.py
+++
@@ -1,5 +0,0 @@
-import urllib.parse
-
-# Object Key
-s3object = urllib.parse.unquote_plus("s3://amazon-connect-b7d5ed773859/connect/itesm2022AmazonConnect/CallRecordings/2022/05/06/7829bcf6-3657-4cef-b77f-8278e9045fb4_20220506T00%3A35_UTC.wav")
-print(s3object)
