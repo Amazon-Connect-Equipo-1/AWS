a897b91882960ffc7bd1b0780b142e2c07ef212f
A01750632 <65176402+A01750632@users.noreply.github.com>
2022-06-01 18:19:03 -0500

    Prueba Fetch
    Prueba Fetch

---
+++ fetchFrontBack/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Jsonfile from './Components/Json';
+
+function App() {
+  return (
+    <div className="App">
+      <Jsonfile />
+    </div>
+  );
+}
+
+export default App;
