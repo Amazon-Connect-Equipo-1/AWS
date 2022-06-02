4cf59638a099961608104e8f938fe59a27729c66
A01750632 <65176402+A01750632@users.noreply.github.com>
2022-06-01 19:55:53 -0500

    change of ec2
    change of ec2

--- fronthttpstest/src/Components/Json.js
+++ fronthttpstest/src/Components/Json.js
@@ -5,7 +5,7 @@ const Jsonfile = () =>{
     const [msg, setMsg]  = useState("");
     const [Error, setError]  = useState(false);
     useEffect(() => {
-        fetch('https://52.37.243.153:8443/principal')
+        fetch('https://35.88.250.238:8082/principal')
         .then(response => response.json())
         .then(data => {
         if (data !== undefined){
